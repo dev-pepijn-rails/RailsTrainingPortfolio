59f9ea6497f3c62e643148894f5870a76c99eb85
Pepijn van de Vorst <Pepijn.vandevorst@gmail.com>
2020-05-05 14:01:41 +0200

    Refactor: whitelist for portfolio in controller concern

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -15,7 +15,7 @@ class PortfoliosController < ApplicationController
 	end
 
 	def create
-    	@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    	@portfolio_item = Portfolio.new(portfolio_params)
     	respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'You portfolio item is now live' }
@@ -32,8 +32,7 @@ class PortfoliosController < ApplicationController
   def update
   	@portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to portfolios_path, notice: 'portfolio_item was successfully updated.' }        
+      if @portfolio_item.update(portfolio_params)   #, notice: 'portfolio_item was successfully updated.'     
       else
         format.html { render :edit }        
       end
@@ -53,6 +52,15 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Portfolio item was successfully destroyed.' }
     end
+  end
+
+  private 
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, 
+                                      :subtitle, 
+                                      :body, 
+                                      technologies_attributes: [:name])
   end
 
 end
