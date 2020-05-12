bb3046acfbe05465286398b739e5f60125370261
Pepijn van de Vorst <Pepijn.vandevorst@gmail.com>
2020-05-12 10:27:25 +0200

    changed to old version of bootstrap and implemented some layout changes on the homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+before_action :set_copyright
+
   def home
   	@posts = Blog.all
   	@skills = Skill.all
@@ -8,5 +10,10 @@ class PagesController < ApplicationController
   end
 
   def contact
+  end
+
+
+  def set_copyright
+  	@copyright  = ViewTool::Renderer.copyright 'Pepijn van de Vorst', 'alle rechten voorbehouden'
   end
 end
