1f210e3ed2ac571f17ff4baec7d07f6752fea93d
Pepijn van de Vorst <Pepijn.vandevorst@gmail.com>
2020-05-18 17:01:41 +0200

    Added htmlsortable javascript file (copy-paste) and jquery ui (using yarn)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,11 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery-ui-dist/jquery-ui")
+require("packs/html.sortable")
+require("turbolinks").start()
 
 import 'bootstrap'
 import './src/application.scss'
