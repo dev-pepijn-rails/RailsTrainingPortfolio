8bc5995b24c5c9f76b1aa220d4ba3804c27e3474
Pepijn van de Vorst <Pepijn.vandevorst@gmail.com>
2020-06-17 16:48:28 +0200

    Added redirect for blog showpage for draft posts so non-admin users can't view draft posts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    if logged_in? (:site_admin)
+    if logged_in?(:site_admin)
        @blogs = Blog.recent.page(params[:page]).per(5)
     else
        @blogs = Blog.published.recent.page(params[:page]).per(5)
@@ -20,10 +20,14 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    @blog = Blog.includes(:comments).friendly.find(params[:id])
-    @comment = Comment.new
-    @page_title = @blog.title
-    @seo_Keywords = @blog.body # niet de juiste manier, normaal gebruik je 6-8 keywords. Dit is ter illustratie
+    if logged_in?(:site_admin) || @blog.published?
+      @blog = Blog.includes(:comments).friendly.find(params[:id])
+      @comment = Comment.new
+      @page_title = @blog.title
+      @seo_Keywords = @blog.body # niet de juiste manier, normaal gebruik je 6-8 keywords. Dit is ter illustratie
+    else
+      redirect_to blogs_path, notice: "You are not authorized to access this page"
+    end
   end
 
   # GET /blogs/new
